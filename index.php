8b4b249360d04419482fa202b958ad3fbfa8cccc
Afanyuy Caleb <afanyuys@gmail.com>
2023-12-23 13:08:32 +0100

    Version 2

--- index.php
+++ index.php
@@ -12,9 +12,11 @@
 </head>
 <body>
 	
+<h2> Hi there and welcome to us trying to create a functional search bar in php</h2>
 	<form action="" method="POST">
 		<input type="text" name="search" placeholder="search electronic">
 	</form>
+
 	<?php
 		if(isset($error)){
 			echo $error;
